57cc849d5fc70b6ac0bd3689233740b3f044c339
ofekdinis <ofekdinis@gmail.com>
2025-07-24 10:20:01 +0300

    Update User.cs
    add required to avoid warnnings

--- Models/User.cs
+++ Models/User.cs
@@ -17,21 +17,21 @@ public class User
     /// Full name of the user.
     /// </summary>
     [Required]
-    public string Name { get; set; }
+    public required string Name { get; set; }
 
     /// <summary>
     /// Phone number used for notifications and communication.
     /// </summary>
     [Required]
     [Phone]
-    public string PhoneNumber { get; set; }
+    public required string PhoneNumber { get; set; }
 
     /// <summary>
     /// Email address used for updates and communication.
     /// </summary>
     [Required]
     [EmailAddress]
-    public string Email { get; set; }
+    public required string Email { get; set; }
 
     /// <summary>
     /// Collection of fields managed by the user.
